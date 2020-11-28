2d5cb3b85ff0338ff27df36da05573bd86ccbb2f
Satrio Teleng Bagaskoro <telengbagaskorosatrio@gmail.com>
2020-11-28 07:39:07 +0700

    Initial Commit

---
+++ swiftui-practice/swiftui_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_practiceApp.swift
+//  swiftui-practice
+//
+//  Created by bagasstb on 28/11/20.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
